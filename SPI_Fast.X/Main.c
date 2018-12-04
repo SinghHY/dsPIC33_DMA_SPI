664e99d90f8178b5eec49f7e813757e3765eb46d
Harpreet Singh <Harpreet.Singh@perkinelmer.com>
2018-12-04 12:27:43 -0500

    Project Added

---
+++ SPI_Fast.X/Main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   Main.c
+ * Author: SinghHy
+ *
+ * Created on December 4, 2018, 12:25 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
