ee5ca59e003cbe364b8b72c244ff2cc92aec140a
Harpreet Singh <Harpreet.Singh@perkinelmer.com>
2018-12-04 12:37:58 -0500

    project modified

--- SPI_Fast.X/Main.c
+++ SPI_Fast.X/Main.c
@@ -1,18 +1,25 @@
-/* 
- * File:   Main.c
- * Author: SinghHy
- *
- * Created on December 4, 2018, 12:25 PM
- */
+/**********************************************************************
+* Filament Control.
+* © Harpreet Singh, 2017
+* FileName:        main.c
+* Dependencies:    Header (.h) files if applicable, see below* 
+* Processor:dsPIC33FJ256GP710A
+* Compiler:        MPLAB® CCS v5.074
+************************************************************************/
+#include <33FJ256GP710A.h>
+#fuses XT,NOWDT,NOPROTECT
+#device ADC = 12 
+#use delay(crystal = 8Mhz)
 
-#include <stdio.h>
-#include <stdlib.h>
 
-/*
- * 
- */
-int main(int argc, char** argv) {
 
-    return (EXIT_SUCCESS);
-}
 
+void main()
+{    
+   output_float(PIN_G9); // SS as an input
+   setup_adc_ports(sAN0 | sAN2 | sAN3| sAN4, VSS_VDD);
+   setup_adc(ADC_CLOCK_INTERNAL);
+
+
+  
+}   
