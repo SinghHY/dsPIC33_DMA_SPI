8340a6ed502f0d039df5d253fa532d23ec3c37bc
Harpreet Singh <Harpreet.Singh@perkinelmer.com>
2018-12-18 17:19:19 -0500

    DMA_CONTINOUS : Not working

--- SPI_Fast.X/Main.c
+++ SPI_Fast.X/Main.c
@@ -10,7 +10,7 @@
 #fuses XT,NOWDT,NOPROTECT
 #device ADC = 12 
 #use delay(crystal = 8Mhz)
-
+#use spi(SLAVE, SPI2, BITS = 8, MODE = 1, ENABLE = PIN_G9, stream = SPI_2)
 
 
 
@@ -19,6 +19,16 @@ void main()
    output_float(PIN_G9); // SS as an input
    setup_adc_ports(sAN0 | sAN2 | sAN3| sAN4, VSS_VDD);
    setup_adc(ADC_CLOCK_INTERNAL);
+   
+   #BANK_DMA 
+   unsigned char SPI2Buff[32]; 
+   unsigned char SPI2OBuff[32];
+
+   setup_dma(6,DMA_OUT_SPI2,DMA_BYTE); 
+   dma_start(6, DMA_CONTINOUS ,&SPI2OBuff[0],32);
+   
+   setup_dma(7, DMA_IN_SPI2, DMA_BYTE);      //use DMA channel 7 for SPI2 DATA IN (dummy) 
+   dma_start(7, DMA_CONTINOUS ,&SPI2Buff[0],32);   //setup dummy receive to avoid SPI2 overrun 
 
 
   
