c3eab9c7e6be5458a5d0fe730c4d65bffc47600f
Harpreet Singh <Harpreet.Singh@perkinelmer.com>
2019-01-07 14:30:37 -0500

    Working version

--- SPI_Fast.X/Main.c
+++ SPI_Fast.X/Main.c
@@ -9,27 +9,42 @@
 #include <33FJ256GP710A.h>
 #fuses XT,NOWDT,NOPROTECT
 #device ADC = 12 
-#use delay(crystal = 8Mhz)
+#use delay(clock = 100Mhz, crystal = 8Mhz)
 #use spi(SLAVE, SPI2, BITS = 8, MODE = 1, ENABLE = PIN_G9, stream = SPI_2)
 
+#define GREEN_LED  PIN_A6
+#define RED_LED  PIN_A7
 
+int8 i, Error;
+   
+#bank_dma 
+    unsigned int8  TxBuffer[4];
+#bank_dma 
+    unsigned int8  RxBuffer[4]; 
 
 void main()
 {    
+    
+   
    output_float(PIN_G9); // SS as an input
-   setup_adc_ports(sAN0 | sAN2 | sAN3| sAN4, VSS_VDD);
-   setup_adc(ADC_CLOCK_INTERNAL);
+
+
+   for(i = 0; i <= 3; i++)
+       TxBuffer[i] = i * 10;
+
+   setup_dma(2, DMA_OUT_SPI2, DMA_BYTE); 
+   dma_start(2, DMA_CONTINOUS ,&TxBuffer[0],3);
    
-   #BANK_DMA 
-   unsigned char SPI2Buff[32]; 
-   unsigned char SPI2OBuff[32];
+   setup_dma(0, DMA_IN_SPI2, DMA_BYTE);   
+   dma_start(0, DMA_CONTINOUS ,&RxBuffer[0],3); 
 
-   setup_dma(6,DMA_OUT_SPI2,DMA_BYTE); 
-   dma_start(6, DMA_CONTINOUS ,&SPI2OBuff[0],32);
-   
-   setup_dma(7, DMA_IN_SPI2, DMA_BYTE);      //use DMA channel 7 for SPI2 DATA IN (dummy) 
-   dma_start(7, DMA_CONTINOUS ,&SPI2Buff[0],32);   //setup dummy receive to avoid SPI2 overrun 
-
-
+   while(1)
+   {
+       output_high(GREEN_LED);
+       delay_ms(500);
+       output_low(GREEN_LED);
+       delay_ms(500);
+       Error = dma_status(0);
+   }
   
 }   
